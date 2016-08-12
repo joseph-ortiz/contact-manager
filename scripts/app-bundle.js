4fb325780ff050b808b95855e4d20f150611c27b
jortiz <josepho011235@gmail.com>
2016-08-12 10:37:55 -0700

    added contact detail section

--- scripts/app-bundle.js
+++ scripts/app-bundle.js
@@ -16,57 +16,6 @@ define('app',["require", "exports"], function (require, exports) {
     exports.App = App;
 });
 
-define('environment',["require", "exports"], function (require, exports) {
-    "use strict";
-    Object.defineProperty(exports, "__esModule", { value: true });
-    exports.default = {
-        debug: true,
-        testing: true
-    };
-});
-
-define('main',["require", "exports", './environment'], function (require, exports, environment_1) {
-    "use strict";
-    Promise.config({
-        warnings: {
-            wForgottenReturn: false
-        }
-    });
-    function configure(aurelia) {
-        aurelia.use
-            .standardConfiguration()
-            .feature('resources');
-        if (environment_1.default.debug) {
-            aurelia.use.developmentLogging();
-        }
-        if (environment_1.default.testing) {
-            aurelia.use.plugin('aurelia-testing');
-        }
-        aurelia.start().then(function () { return aurelia.setRoot(); });
-    }
-    exports.configure = configure;
-});
-
-define('no-selection',["require", "exports"], function (require, exports) {
-    "use strict";
-    var NoSelection = (function () {
-        function NoSelection() {
-            this.message = "Please Select a Contact.";
-        }
-        return NoSelection;
-    }());
-    exports.NoSelection = NoSelection;
-});
-
-define('utility',["require", "exports"], function (require, exports) {
-    "use strict";
-    function areEqual(obj1, obj2) {
-        return Object.keys(obj1).every(function (key) { return obj2.hasOwnProperty(key) && (obj1[key] === obj2[key]); });
-    }
-    exports.areEqual = areEqual;
-    ;
-});
-
 define('web-api',["require", "exports"], function (require, exports) {
     "use strict";
     var latency = 200;
@@ -169,6 +118,144 @@ define('web-api',["require", "exports"], function (require, exports) {
     exports.WebAPI = WebAPI;
 });
 
+define('utility',["require", "exports"], function (require, exports) {
+    "use strict";
+    function areEqual(obj1, obj2) {
+        return Object.keys(obj1).every(function (key) { return obj2.hasOwnProperty(key) && (obj1[key] === obj2[key]); });
+    }
+    exports.areEqual = areEqual;
+    ;
+});
+
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+define('contact-detail',["require", "exports", 'aurelia-framework', './web-api', './utility'], function (require, exports, aurelia_framework_1, web_api_1, utility_1) {
+    "use strict";
+    var ContactDetail = (function () {
+        function ContactDetail(api) {
+            this.api = api;
+        }
+        ContactDetail.prototype.activate = function (params, routeConfig) {
+            var _this = this;
+            this.routeConfig = routeConfig;
+            return this.api.getContactDetails(params.id).then(function (contact) {
+                _this.contact = contact;
+                _this.routeConfig.navModel.setTitle(_this.contact.firstName);
+                _this.originalContact = JSON.parse(JSON.stringify(_this.contact));
+            });
+        };
+        Object.defineProperty(ContactDetail.prototype, "canSave", {
+            get: function () {
+                return this.contact.firstName && this.contact.lastName && !this.api.isRequesting;
+            },
+            enumerable: true,
+            configurable: true
+        });
+        ContactDetail.prototype.save = function () {
+            var _this = this;
+            this.api.saveContact(this.contact).then(function (contact) {
+                _this.contact = contact;
+                _this.routeConfig.navModel.setTitle(_this.contact.firstName);
+                _this.originalContact = JSON.parse(JSON.stringify(contact));
+            });
+        };
+        ContactDetail.prototype.canDeactivate = function () {
+            if (!utility_1.areEqual(this.originalContact, this.contact)) {
+                return confirm('You have unsaved changes. Are you sure you wish to leave?');
+            }
+            return true;
+        };
+        ContactDetail = __decorate([
+            aurelia_framework_1.inject(web_api_1.WebAPI), 
+            __metadata('design:paramtypes', [web_api_1.WebAPI])
+        ], ContactDetail);
+        return ContactDetail;
+    }());
+    exports.ContactDetail = ContactDetail;
+});
+
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+define('contact-list',["require", "exports", './web-api', 'aurelia-framework'], function (require, exports, web_api_1, aurelia_framework_1) {
+    "use strict";
+    var ContactList = (function () {
+        function ContactList(api) {
+            this.api = api;
+            this.selectedId = 0;
+        }
+        ContactList.prototype.created = function () {
+            var _this = this;
+            this.api.getContactList().then(function (contacts) { return _this.contacts = contacts; });
+        };
+        ContactList.prototype.select = function (contact) {
+            this.selectedId = contact.id;
+            return true;
+        };
+        ContactList = __decorate([
+            aurelia_framework_1.inject(web_api_1.WebAPI), 
+            __metadata('design:paramtypes', [web_api_1.WebAPI])
+        ], ContactList);
+        return ContactList;
+    }());
+    exports.ContactList = ContactList;
+});
+
+define('environment',["require", "exports"], function (require, exports) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    exports.default = {
+        debug: true,
+        testing: true
+    };
+});
+
+define('main',["require", "exports", './environment'], function (require, exports, environment_1) {
+    "use strict";
+    Promise.config({
+        warnings: {
+            wForgottenReturn: false
+        }
+    });
+    function configure(aurelia) {
+        aurelia.use
+            .standardConfiguration()
+            .feature('resources');
+        if (environment_1.default.debug) {
+            aurelia.use.developmentLogging();
+        }
+        if (environment_1.default.testing) {
+            aurelia.use.plugin('aurelia-testing');
+        }
+        aurelia.start().then(function () { return aurelia.setRoot(); });
+    }
+    exports.configure = configure;
+});
+
+define('no-selection',["require", "exports"], function (require, exports) {
+    "use strict";
+    var NoSelection = (function () {
+        function NoSelection() {
+            this.message = "Please Select a Contact.";
+        }
+        return NoSelection;
+    }());
+    exports.NoSelection = NoSelection;
+});
+
 define('resources/index',["require", "exports"], function (require, exports) {
     "use strict";
     function configure(config) {
@@ -176,7 +263,9 @@ define('resources/index',["require", "exports"], function (require, exports) {
     exports.configure = configure;
 });
 
-define('text!app.html', ['module'], function(module) { module.exports = "<template>\n  <require from=\"bootstrap/css/bootstrap.css\"></require>\n  <require from=\"./styles.css\"></require>\n\n  <nav class=\"navbar navbar-default navbar-fixed-top\" role=\"navigation\">\n    <div class=\"navbar-header\">\n      <a class=\"navbar-brand\" href=\"#\">\n        <i class=\"fa fa-user\"></i>\n        <span>Contacts</span>\n      </a>\n    </div>\n  </nav>\n\n  <div class=\"container\">\n    <div class=\"row\">\n      <div class=\"col-md-4\">Contact List Placeholder</div>\n      <router-view class=\"col-md-8\"></router-view>\n    </div>\n  </div>\n</template>"; });
+define('text!app.html', ['module'], function(module) { module.exports = "<template>\n  <require from=\"bootstrap/css/bootstrap.css\"></require>\n  <require from=\"./styles.css\"></require>\n  <require from=\"./contact-list\"></require>\n  \n\n  <nav class=\"navbar navbar-default navbar-fixed-top\" role=\"navigation\">\n    <div class=\"navbar-header\">\n      <a class=\"navbar-brand\" href=\"#\">\n        <i class=\"fa fa-user\"></i>\n        <span>Contacts</span>\n      </a>\n    </div>\n  </nav>\n\n  <div class=\"container\">\n    <div class=\"row\">\n      <contact-list class=\"col-md-4\"></contact-list>\n      <router-view class=\"col-md-8\"></router-view>\n    </div>\n  </div>\n</template>"; });
 define('text!styles.css', ['module'], function(module) { module.exports = "body { padding-top: 70px; }\n\nsection {\n  margin: 0 20px;\n}\n\na:focus {\n  outline: none;\n}\n\n.navbar-nav li.loader {\n    margin: 12px 24px 0 6px;\n}\n\n.no-selection {\n  margin: 20px;\n}\n\n.contact-list {\n  overflow-y: auto;\n  border: 1px solid #ddd;\n  padding: 10px;\n}\n\n.panel {\n  margin: 20px;\n}\n\n.button-bar {\n  right: 0;\n  left: 0;\n  bottom: 0;\n  border-top: 1px solid #ddd;\n  background: white;\n}\n\n.button-bar > button {\n  float: right;\n  margin: 20px;\n}\n\nli.list-group-item {\n  list-style: none;\n}\n\nli.list-group-item > a {\n  text-decoration: none;\n}\n\nli.list-group-item.active > a {\n  color: white;\n}\n"; });
-define('text!no-selection.html', ['module'], function(module) { module.exports = "<template>\n  <div class=\"no-selection text-center\">\n    <h2>${message}</h2>\n  </div>\n</template>"; });
+define('text!contact-detail.html', ['module'], function(module) { module.exports = "<template>\n  <div class=\"panel panel-primary\">\n    <div class=\"panel-heading\">\n      <h3 class=\"panel-title\">Profile</h3>\n    </div>\n    <div class=\"panel-body\">\n      <form role=\"form\" class=\"form-horizontal\">\n        <div class=\"form-group\">\n          <label class=\"col-sm-2 control-label\">First Name</label>\n          <div class=\"col-sm-10\">\n            <input type=\"text\" placeholder=\"first name\" class=\"form-control\" value.bind=\"contact.firstName\">\n          </div>\n        </div>\n\n        <div class=\"form-group\">\n          <label class=\"col-sm-2 control-label\">Last Name</label>\n          <div class=\"col-sm-10\">\n            <input type=\"text\" placeholder=\"last name\" class=\"form-control\" value.bind=\"contact.lastName\">\n          </div>\n        </div>\n\n        <div class=\"form-group\">\n          <label class=\"col-sm-2 control-label\">Email</label>\n          <div class=\"col-sm-10\">\n            <input type=\"text\" placeholder=\"email\" class=\"form-control\" value.bind=\"contact.email\">\n          </div>\n        </div>\n\n        <div class=\"form-group\">\n          <label class=\"col-sm-2 control-label\">Phone Number</label>\n          <div class=\"col-sm-10\">\n            <input type=\"text\" placeholder=\"phone number\" class=\"form-control\" value.bind=\"contact.phoneNumber\">\n          </div>\n        </div>\n      </form>\n    </div>\n  </div>\n\n  <div class=\"button-bar\">\n    <button class=\"btn btn-success\" click.delegate=\"save()\" disabled.bind=\"!canSave\">Save</button>\n  </div>\n</template>"; });
+define('text!contact-list.html', ['module'], function(module) { module.exports = "<template>\n  <div class=\"contact-list\">\n    <ul class=\"list-group\">\n      <li repeat.for=\"contact of contacts\" class=\"list-group-item ${contact.id === $parent.selectedId ? 'active' : ''}\">\n        <a route-href=\"route: contacts; params.bind: {id:contact.id}\" click.delegate=\"$parent.select(contact)\">\n          <h4 class=\"list-group-item-heading\">${contact.firstName} ${contact.lastName}</h4>\n          <p class=\"list-group-item-text\">${contact.email}</p>\n        </a>\n      </li>\n    </ul>\n  </div>\n</template>"; });
+define('text!no-selection.html', ['module'], function(module) { module.exports = "<template>\n  <div class=\"contact-list\">\n    <ul class=\"list-group\">\n      <li repeat.for=\"contact of contacts\" class=\"list-group-item ${contact.id === $parent.selectedId ? 'active' : ''}\">\n        <a route-href=\"route: contacts; params.bind: {id:contact.id}\" click.delegate=\"$parent.select(contact)\">\n          <h4 class=\"list-group-item-heading\">${contact.firstName} ${contact.lastName}</h4>\n          <p class=\"list-group-item-text\">${contact.email}</p>\n        </a>\n      </li>\n    </ul>\n  </div>\n</template>"; });
 //# sourceMappingURL=app-bundle.js.map
